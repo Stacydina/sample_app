61b23788034ed3b20b00e410312bd27fcbab6e90
Stacydina <stacydina6@gmail.com>
2018-02-13 09:46:47 +0300

    Rails flavoured ruby

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -10,4 +10,8 @@ class StaticPagesController < ApplicationController
 
   def contact
   end
+
+  
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-root 'static_pages#home'
+  #root 'static_pages#home'
 
   get 'static_pages/home'
 
@@ -8,6 +8,10 @@ root 'static_pages#home'
   get 'static_pages/about'
 
   get 'static_pages/contact'
+
+  
+ 
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #root 'application#hello'
