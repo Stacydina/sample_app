fd7172a1fff0f180c1336e300efbe0bcdcfac420
Stacydina <stacydina6@gmail.com>
2018-02-23 11:29:13 +0300

    Lenght validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ApplicationRecord
-    attr_accessor :name, :email
-def initialize(attributes = {})
-@name = attributes[:name]
-@email = attributes[:email]
+    validates :name, presence: true, length: {maximum: 50}
+    validates :email, presence: true, length: {maximum: 255}
 end
